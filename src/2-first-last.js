99f0a27e983fcffd01286701e623b22784a65e66
Agnes Harrysson Hansson <aagneshansson@hotmail.com>
2020-10-11 18:20:09 +0200

    number two

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,9 @@
 export const firstLast = (items) => {
+  if (items.lenght ===0) {
+    return 'No items!'
+  }
+  else if (items.lenght === 1) {
+    return `Only item: ${items[0]}`
+  }
   return `First: ${items[0]}, Last: ${items[1]}`
 }
